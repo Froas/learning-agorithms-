b8f5017b8c6a13850c0eb650cff48e34d8cf037a
Froas <froas.crp@gmail.com>
2021-08-26 01:09:53 +0600

    some fix

--- sort/bubble.ts
+++ sort/bubble.ts
@@ -1,27 +1,19 @@
-const numArr = [5, 7, 4, 3, 8, 2]
-const numArr2 = [1, 44, 3, 3, 1, 0, 4, 5]
-const numArr3 = [7, 2, 0, 99, 1, 3]
-const numArr4 = [6, 1, 9, 12, 3, 7, 5, 3, 0]
-const con = 0
+const arr = [5, 7, 4, 3, 8, 2]
+const counter = 0
 
-function bubble(arr, con){
+function bubble(arr, cont){
   
-  let count = [con]
+  let count = [cont]
   const newaArr = []
   
-  const bub = arr.reduce((acc, curr) => acc > curr 
+  const sort = arr.reduce((acc, curr) => acc > curr 
     ? (newaArr.pop(), newaArr.push(curr), newaArr.push(acc), count.push(1), acc) 
     : (newaArr.push(curr),curr), arr[0])
   
-    const counted = count.reduce((x, y) => x + y)
+  const counted = count.reduce((x, y) => x + y)
   const equals = (a, b) => JSON.stringify(a) === JSON.stringify(b)
-  const rec = equals(arr,newaArr) ? [newaArr, counted] : bubble(newaArr, counted)
-  return rec
+  const recur = equals(arr,newaArr) ? [newaArr, counted] : bubble(newaArr, counted)
+  return recur
   
 }
-console.log(bubble(numArr, con))
-console.log(bubble(numArr2, con))
-console.log(bubble(numArr3, con))
-console.log(bubble(numArr4, con))
-
-// slice spice
+console.log(bubble(arr, counter))
